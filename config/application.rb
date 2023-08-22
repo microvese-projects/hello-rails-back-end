31d8d31a6a85cf865fd4240e2e9e71c48dfc4158
Benson <njugunab655@gmail.com>
2023-08-22 17:38:48 +0300

    Config the cors restrictions

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module HelloRailsBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000' # Replace with your React app's URL
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
